41b3242a2d87ddfd20d323111780c31ef2b7aa17
mdmamunfullstack <mamun9398263@gmail.com>
2021-08-17 23:16:42 +0600

    Add project files.

---
+++ MyShop.Web/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MyShop.Domain.Models;
+using MyShop.Infrastructure;
+using MyShop.Infrastructure.Repositories;
+
+namespace MyShop.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly IRepository<Customer> customerRepository;
+
+        public CustomerController(IRepository<Customer> customerRepository)
+        {
+            this.customerRepository = customerRepository;
+        }
+
+        public IActionResult Index(Guid? id)
+        {
+            if (id == null)
+            {
+                var customers = customerRepository.All();
+
+                return View(customers);
+            }
+            else
+            {
+                var customer = customerRepository.Get(id.Value);
+
+                return View(new[] { customer });
+            }
+        }
+    }
+}
